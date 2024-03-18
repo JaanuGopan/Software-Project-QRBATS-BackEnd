f30cc1e14a79928be2d474b90643392144f420a8
JaanuGopan <sjanugopan@gmail.com>
2024-03-18 14:55:57 +0530

    update commit

--- src/main/java/com/qrbats/qrbats/authentication/controller/AuthenticationController.java
+++ src/main/java/com/qrbats/qrbats/authentication/controller/AuthenticationController.java
@@ -8,14 +8,12 @@ import com.qrbats.qrbats.authentication.entities.user.User;
 import com.qrbats.qrbats.authentication.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000/")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
--- src/main/java/com/qrbats/qrbats/functionalities/attendance/AttendanceMarkingController.java
+++ src/main/java/com/qrbats/qrbats/functionalities/attendance/AttendanceMarkingController.java
@@ -3,14 +3,12 @@ package com.qrbats.qrbats.functionalities.attendance;
 import com.qrbats.qrbats.entity.attendance.Attendance;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/attendance")
 @AllArgsConstructor
+@CrossOrigin("http://localhost:3000/")
 public class AttendanceMarkingController {
     private final AttendanceMarkingService attendanceMarkingService;
     @PostMapping("markattendance")
