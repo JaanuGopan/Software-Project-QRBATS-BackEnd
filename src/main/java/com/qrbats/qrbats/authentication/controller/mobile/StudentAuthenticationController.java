91b748e179a42792c2e08aa96b27e17136ef591d
Janugopan Sundaramoorthy <118627952+JaanuGopan@users.noreply.github.com>
2025-02-09 23:35:53 +0530

    Update StudentAuthenticationController.java

--- src/main/java/com/qrbats/qrbats/authentication/controller/mobile/StudentAuthenticationController.java
+++ src/main/java/com/qrbats/qrbats/authentication/controller/mobile/StudentAuthenticationController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/mobile")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class StudentAuthenticationController {
 
     private final MobileAuthenticationService mobileAuthenticationService;
