f8b10e64fc9ea8a45c7435dbe813c22764abfbe7
Janugopan Sundaramoorthy <118627952+JaanuGopan@users.noreply.github.com>
2025-02-09 23:33:19 +0530

    Update WebConfig.java

--- src/main/java/com/qrbats/qrbats/config/WebConfig.java
+++ src/main/java/com/qrbats/qrbats/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173","http://10.50.227.44:5000")
+                .allowedOrigins("http://localhost:5173","http://10.50.227.44:5000","http://localhost:52249")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
